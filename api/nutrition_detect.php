c69ef42ffa6bc360380adb605400328540a6ae89
Chris-calm <casimirochris19@gmail.com>
2026-01-01 16:47:10 +0800

    fix: Remove deprecated curl_close function call

--- api/nutrition_detect.php
+++ api/nutrition_detect.php
@@ -73,7 +73,6 @@ try {
 
     $response = curl_exec($ch);
     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
 
     if ($httpCode !== 200) {
         throw new Exception('Failed to communicate with OpenAI API. HTTP status: ' . $httpCode . ' Response: ' . $response);
